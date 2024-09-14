6ccfc1ac46cfc729e5df6309ef621153691645f9
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2024-09-14 15:07:18 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+void setup() {
+// write your initialization code here
+    Serial.begin(9600);
+}
+
+void loop() {
+    Serial.println("hi");
+    Serial.print(Serial.read());
+    delay(1000);
+// write your code here
+}
