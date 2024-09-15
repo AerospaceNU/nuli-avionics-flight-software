d75dfc26a7c497a6391709e44e1b80a2d4dff62d
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2024-09-14 22:49:22 -0400

    a

--- platformio_nuli_avionics_flight_software/src/main.cpp
+++ platformio_nuli_avionics_flight_software/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 void setup() {
-// write your initialization code here
+    // write your initialization code here
     Serial.begin(9600);
 }
 
@@ -9,5 +9,5 @@ void loop() {
     Serial.println("hi");
     Serial.print(Serial.read());
     delay(1000);
-// write your code here
+    // write your code here
 }
