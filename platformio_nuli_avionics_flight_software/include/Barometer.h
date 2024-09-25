7aa123dbf32265eead6f639899c5db886f40e3a3
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2024-09-24 23:00:45 -0400

    Docs

--- platformio_nuli_avionics_flight_software/include/Barometer.h
+++ platformio_nuli_avionics_flight_software/include/Barometer.h
@@ -8,19 +8,36 @@ class Barometer : public BaseSensor {
 public:
     void inject(double temperatureC = 0, double humidity = 0, double pressureAtm = 0) {
         m_temperatureC = temperatureC;
-        m_humidity = humidity;
+        m_humidityPercent = humidity;
         m_pressureAtm = pressureAtm;
         calculateAltitude();
     }
 
+    inline double getTemperatureC() const {
+        return m_temperatureC;
+    }
+
+    inline double getHumidityPercent() const {
+        return m_humidityPercent;
+    }
+
+    inline double getPressureAtm() const {
+        return m_pressureAtm;
+    }
+
+    inline double getAltitudeM() const {
+        return m_altitudeM;
+    }
+
 protected:
     void calculateAltitude() {
+        // @todo add
 //        (tempRef / lapseRate) *
 //        (pow(presAvg / presRef, -R_DRY_AIR * lapseRate / G_ACCEL_EARTH) - 1);
     }
 
     double m_temperatureC = 0;
-    double m_humidity = 0;
+    double m_humidityPercent = 0;
     double m_pressureAtm = 0;
     double m_altitudeM = 0;
 };
