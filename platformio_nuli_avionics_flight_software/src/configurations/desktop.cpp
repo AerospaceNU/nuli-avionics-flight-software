aebcfb2c86e56c3f7ce21baaadaf0dc9ae1c1d4d
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2024-11-13 21:54:08 -0500

    update

--- platformio_nuli_avionics_flight_software/src/configurations/desktop.cpp
+++ platformio_nuli_avionics_flight_software/src/configurations/desktop.cpp
@@ -1,53 +1,42 @@
-//#include <cmath>
-//#include <cstring>
-//
-//#include "../drivers/samd21/AprsModulation.h"
-//
-//int main() {
-//    AprsModulation aprsModulation(1);
-////    aprsModulation.setup()
-//    const char* str = "asdf345";
-//    aprsModulation.transmit(str, strlen(str));
-//    std::cout << "\n";
-//}
+#include <Avionics.h>
+#include <GenericSensor.h>
+#include <Barometer.h>
+#include <Accelerometer.h>
+#include <GPS.h>
+#include <Gyroscope.h>
+#include <Magnetometer.h>
+#include <Pyro.h>
+#include <AvionicsCore.h>
+#include <HardwareAbstraction.h>
+#include <Filters.h>
+#include <FlashMemory.h>
+#include <CommunicationLink.h>
 
-//const double ATMOSPHERIC_PRESSURE_PA = 101325;
-//const double GRAVITATIONAL_CONSTANT_M_SS = 9.80665;
-//const double LAPSE_RATE_CONSTANT_K_M = 0.0065;
-//const double GAS_CONSTANT_J_KG_K = 287.0474909;
-//
-//double calculateAltitude(double m_temperatureK, double m_pressurePa) {
-//    return (m_temperatureK / LAPSE_RATE_CONSTANT_K_M) *
-//           (1 - pow(m_pressurePa / ATMOSPHERIC_PRESSURE_PA, (GAS_CONSTANT_J_KG_K * LAPSE_RATE_CONSTANT_K_M) / GRAVITATIONAL_CONSTANT_M_SS));
-//
-//}
-//
-//double calculateAltitude2(double m_temperatureK, double m_pressurePa) {
-//    // @todo add
-//    return (m_temperatureK / -LAPSE_RATE_CONSTANT_K_M) *
-//           (pow(m_pressurePa / ATMOSPHERIC_PRESSURE_PA, -(GAS_CONSTANT_J_KG_K * -LAPSE_RATE_CONSTANT_K_M) / GRAVITATIONAL_CONSTANT_M_SS) - 1);
-//}
-//
-//int main() {
-//    for(double a = ATMOSPHERIC_PRESSURE_PA; a > ATMOSPHERIC_PRESSURE_PA * .6; a -= 10) {
-//        std::cout << calculateAltitude(293, a) << " : " << calculateAltitude2(293, a) << "\n";
-//    }
-//}
+Barometer barometer;
+Accelerometer accelerometer;
+Gyroscope gyroscope;
+Magnetometer magnetometer;
 
+// Core objects accessible by all components
+HardwareAbstraction hardware;
+Configuration configuration;
+Logger logger;
+Filters filter;
+AvionicsCore avionicsCore;
 
-/**
-* Notes:
- * - States should be their own thing class?
- * - Sensor data:
- *      -
- *
- * Abstract Sensor Class:
- *  - Raw data X #ofSensors
- *  - Calculated data (degs, alt, pos)
- *  Further Abstracted Sensor Class????
- * - Combined data (1XSensor)
- * Processing Class:
- *  - Filtered data
- *  - Outputs struct or API
- *
-*/
+int main(int argc, char *argv[]) {
+    hardware.addBarometer(&barometer);
+    hardware.addAccelerometer(&accelerometer);
+    hardware.addGyroscope(&gyroscope);
+    hardware.addMagnetometer(&magnetometer);
+
+    hardware.setup();
+    configuration.setup(&hardware);
+    logger.setup(&hardware);
+    filter.setup(&configuration, &logger);
+    avionicsCore.setup(&hardware, &configuration, &logger, &filter);
+
+    while (true) {
+        avionicsCore.loopOnce();
+    }
+}
