c19d0a08e248b0c17d352542b5ffde3b84218b12
Riley Post <post.ri@northeastern.edu>
2026-01-19 03:16:17 -0500

    uhhh like halfway through the lsm driver

--- platformio_nuli_avionics_flight_software/src/drivers/arduino/LSM6DSV320XSensor.cpp
+++ platformio_nuli_avionics_flight_software/src/drivers/arduino/LSM6DSV320XSensor.cpp
@@ -1,10 +1,103 @@
+#include <Arduino.h>
+#include "LSM6DSV320XSensor.h"
+#include <SPI.h>
 
-// #define WHO_AM_I_REGISTER 0x0f;
-// #define PIN_CTRL_REGISTER 0x02; //need to set IO_PAD_STRENGTH[1:0] to 00
-// #define IF_CFG_REGISTER 0x03; //can be used to disable all interfaces except for SPI
-// #define CTRL1_REGISTER 0x10; // Accelerometer operating mode
-// #define CTRL2_REGISTER 0x11; // Gyroscope operating mode
-// #define STATUS_REGISTER 0x1E; //Status register (Data Ready)
-// #define OUT_TEMP_REGISTER_START 0x20; // 0x20L, 0x21H
-// #define OUT_GYRO_REGISTER_START 0x22; //Gyroscope data: X L/H, Y L/H, Z L/H     0x22 .. 0x27
-// #define OUT_ACCEL_REGISTER_START 0x28; //X L/H, Y L/H, Z L/H     0x28 .. 0x2D
+// Basic device register addresses
+#define WHO_AM_I_REG       0x0F  // Contains device ID
+#define CTRL1_XL_REG       0x10  // Low G Accelerometer control
+#define CTRL2_G_REG        0x11  // Gyroscope control
+#define CTRL1_XL_HG        0x4E  // High G Accelerometer control
+#define CTRL6              0x15  // set dps & LPF of Gyro
+#define CTRL8              0x17  // set Accel low G +- selection 0x03
+#define OUTX_L_A           0x28  // Low G Accel X low byte (multi-byte)
+#define UI_OUTX_L_A_OIS_HG 0x34  // High G Accel X low byte (muli-byte)
+#define OUTX_L_G           0x22  // Gyro X low byte (multi-byte)
+
+
+LSM6DSV320XSensor::LSM6DSV320XSensor(const Vector3DTransform* transform, const uint8_t chipSelectPin, SPIClass *spi) :
+    m_highGAccelerometer(transform),
+    m_lowGAccelerometer(transform),
+    m_gyroscope(transform),
+    m_chipSelectPin(chipSelectPin),
+    m_spi(spi) {}
+
+void LSM6DSV320XSensor::setup(DebugStream* debugStream) {
+    pinMode(m_chipSelectPin, OUTPUT);
+    digitalWrite(m_chipSelectPin, HIGH);
+    m_spi->begin();
+
+    //setting up registers
+
+    // probably don't need these first two as they are defaults
+    writeRegister(CTRL1_XL_REG, 0x00); // Low G Accel power down
+    writeRegister(CTRL1_XL_HG, 0x00);  // High G Accel power down
+    writeRegister(CTRL2_G_REG, 0x00);  // Gyro Power down
+
+    // Configure Sensors
+
+    // +- 16G Low-G Accel
+    writeRegister(CTRL8, 0x03);
+    // Gyro +-4000 dps
+    writeRegister(CTRL6, 0x0D);
+
+    // Low G High performance 120Hz Accel 16 G
+    writeRegister(CTRL1_XL_REG, 0x06);
+    // High G High performance 480Hz Accel 320 G
+    writeRegister(CTRL1_XL_HG, 0x9C);
+    // High performance 120Hz Gyro 4000 dps
+    writeRegister(CTRL2_G_REG, 0x06);
+    uint8_t whoAmI = readRegister(WHO_AM_I_REG);
+    if (whoAmI == 0x73) {
+        debugStream->message("LSM6DSV320X initialized");
+    }
+    else {
+        debugStream->message("LSM6DSV320X initialization failed, unexpected device ID: %d", whoAmI);
+    }
+}
+
+
+void LSM6DSV320XSensor::read() {
+
+}
+
+Accelerometer* LSM6DSV320XSensor::getHighGAccelerometer() {
+    return &m_highGAccelerometer;
+}
+
+Accelerometer* LSM6DSV320XSensor::getLowGAccelerometer() {
+    return &m_lowGAccelerometer;
+}
+
+Gyroscope* LSM6DSV320XSensor::getGyroscope() {
+    return &m_gyroscope;
+}
+
+void LSM6DSV320XSensor::writeRegister(uint8_t reg, uint8_t value) {
+    digitalWrite(m_chipSelectPin, LOW);
+    SPI.transfer(reg & 0x7F);  // Clear MSB for write
+    SPI.transfer(value);
+    digitalWrite(m_chipSelectPin, HIGH);
+}
+
+uint8_t LSM6DSV320XSensor::readRegister(uint8_t reg) {
+    digitalWrite(m_chipSelectPin, LOW);
+    SPI.transfer(0x80 | reg);  // Set MSB for read
+    uint8_t val = SPI.transfer(0x00);
+    digitalWrite(m_chipSelectPin, HIGH);
+    return val;
+}
+
+// Read multiple bytes
+void LSM6DSV320XSensor::readBytes(uint8_t reg, uint8_t *buffer, uint8_t length) {
+    digitalWrite(m_chipSelectPin, LOW);
+    SPI.transfer(0x80 | reg);  // Read (auto increment set up by default)
+    for (uint8_t i = 0; i < length; i++) {
+        buffer[i] = SPI.transfer(0x00);
+    }
+    digitalWrite(m_chipSelectPin, HIGH);
+}
+
+
+
+
+
--- platformio_nuli_avionics_flight_software/src/drivers/arduino/LSM6DSV320XSensor.h
+++ platformio_nuli_avionics_flight_software/src/drivers/arduino/LSM6DSV320XSensor.h
@@ -1,12 +1,89 @@
 #ifndef PLATFORMIO_NULI_AVIONICS_FLIGHT_SOFTWARE_LSM6DSV320XSENSOR_H
 #define PLATFORMIO_NULI_AVIONICS_FLIGHT_SOFTWARE_LSM6DSV320XSENSOR_H
 
+#include <Arduino.h>
 #include <Avionics.h>
+#include <SPI.h>
+
 #include "../../core/generic_hardware/GenericSensor.h"
+#include "core/generic_hardware/Accelerometer.h"
+#include "core/generic_hardware/Gyroscope.h"
+#include "core/transform/Vector3DTransform.h"
 
 
+
+
+/**
+ * @class LSM6DSV320X Driver
+ * @brief driver for LSM6DSV320X
+ * @details driver for SPI connection only
+ */
 class LSM6DSV320XSensor final : public GenericSensor {
+public:
 
+    /**
+     * @brief create a LSM6DSV320X Object
+     * @param transform -> transform into board coordinates
+     */
+    explicit LSM6DSV320XSensor(const Vector3DTransform *transform, const uint8_t chipSelectPin, SPIClass *spi);
+
+    /**
+     * @brief Initializes the sensor
+     * @details Starts communication, and configures parameters
+     */
+    void setup(DebugStream *debugStream) override;
+
+    /**
+     * @brief Reads data from the sensor
+     * @details This uses injector classes, meaning that the data is read in, but then passed into "dummy" classes.
+     * These "dummy" classes are what are actually used by the rest of the code to access data.
+     */
+    void read() override;
+
+    /**
+     * @brief Gets the High G Accelerometer injector class
+     * @details Injector classes are used to pass data to the HardwareAbstraction.
+     * The LSM is basically a 3 in one sensor, so we make a "dummy" class for each of the 3 sensors, and pass data through it.
+     * @return High G Accelerometer injector class
+     */
+    Accelerometer* getHighGAccelerometer();
+
+    /**
+     * @brief Gets the Low G Accelerometer injector class
+     * @details Injector classes are used to pass data to the HardwareAbstraction.
+     * The LSM is basically a 3 in one sensor, so we make a "dummy" class for each of the 3 sensors, and pass data through it.
+     * @return Low G Accelerometer injector class
+     */
+    Accelerometer* getLowGAccelerometer();
+
+    /**
+     * @brief Gets the Gyroscope injector class
+     * @details Injector classes are used to pass data to the HardwareAbstraction.
+     * The ICM20602 is basically a 3 in one sensor, so we make a "dummy" class for each of the 3 sensors, and pass data through it.
+     * @return Gyroscope injector class
+     */
+    Gyroscope* getGyroscope();
+
+private:
+    // Injector Classes
+    Accelerometer m_highGAccelerometer;
+    Accelerometer m_lowGAccelerometer;
+    Gyroscope m_gyroscope;
+
+    //SPI
+    uint8_t m_chipSelectPin;
+    SPIClass *m_spi;
+
+    // Data
+    int16_t highGAccelData[3] = {};
+    int16_t lowGAccelData[3] = {};
+    int16_t gyroData[3] = {};
+    int16_t tempRaw = 0;
+
+    // internal functions
+    void writeRegister(uint8_t reg, uint8_t value);
+    uint8_t readRegister(uint8_t reg);
+    void readBytes(uint8_t reg, uint8_t *buffer, uint8_t length);
 };
 
 
